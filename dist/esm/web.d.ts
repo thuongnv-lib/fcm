3aeb2b961dfa32ba15833ef7e102c01b1ec34e05
Thuong Nguyen Van <nvthuong1996@gmail.com>
2022-01-23 20:57:24 +0700

    add dist folder

---
+++ dist/esm/web.d.ts
@@ -0,0 +1,28 @@
+import { WebPlugin } from '@capacitor/core';
+import { FCMPlugin } from './definitions';
+export declare class FCMWeb extends WebPlugin implements FCMPlugin {
+    constructor();
+    subscribeTo(_options: {
+        topic: string;
+    }): Promise<{
+        message: string;
+    }>;
+    unsubscribeFrom(_options: {
+        topic: string;
+    }): Promise<{
+        message: string;
+    }>;
+    getToken(): Promise<{
+        token: string;
+    }>;
+    deleteToken(): Promise<boolean>;
+    deleteInstance(): Promise<boolean>;
+    setAutoInit(_options: {
+        enabled: boolean;
+    }): Promise<void>;
+    isAutoInitEnabled(): Promise<{
+        enabled: boolean;
+    }>;
+}
+declare const FCM: FCMWeb;
+export { FCM };
